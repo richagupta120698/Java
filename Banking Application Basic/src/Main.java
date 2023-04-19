3c1ab2acc2ee1e086667c683a722f55f14c142e7
Richa Gupta <85608709+richagupta120698@users.noreply.github.com>
2023-04-19 09:44:51 +0530

    Update Main.java

--- Banking Application Basic/src/Main.java
+++ Banking Application Basic/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Account account1 = new Account("100001",
-                "Richa Gupta", "richa@gmail.com", "9765876534", 100000);
+                "Arun", "richa@gmail.com", "9765876534", 100000);
 
 
         account1.withdrawMoney(10000000);
